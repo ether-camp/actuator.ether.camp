8f5d36f168792f98c3544aeacf19be3ecc2c41ad
Dmitry Shmatko <zilm@zilm.net>
2016-10-11 16:10:50 +0600

    Added random source

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "SERVER_PORT: $SERVER_PORT"
 echo "MANAGE_PASS: $MANAGE_PASS"
 
-./gradlew clean bootRun -Dserver.port=${SERVER_PORT} -Dmanage.pass=${MANAGE_PASS} > out.log &
+./gradlew clean bootRun -Djava.security.egd=file:/dev/./urandom -Dserver.port=${SERVER_PORT} -Dmanage.pass=${MANAGE_PASS} > out.log &
